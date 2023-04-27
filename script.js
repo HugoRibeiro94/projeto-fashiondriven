1a769bcdadcf4f40d6c8f795321912921d7723ef
hugo <hugoo.ribeiro03@gmail.com>
2023-04-27 08:22:55 -0300

    versao com axios

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+axios.defaults.headers.common['Authorization'] = 'jPkEcGwk9mO6MJW5ja4IIlL9';
+
+let usuario = prompt('Digite seu nome:');
 
 function nomeUsuario(){
-    let usuario = prompt('Digite seu nome:');
+    
     const nome = document.querySelector('.usuario');
     nome.innerHTML = `Olá, ${usuario}!`;
 }
 nomeUsuario()
-function digita(){
+
+/*function digita(){
     const i = document.querySelector('input');
     i.classList.add('selecionado');
-}
+}*/
+
 let modeloEscolhido = '';
 let golaEscolhido = '';
 let tecidoEscolhido = '';
@@ -27,26 +32,8 @@ function verificaSeleçao(){
             }
         }
     }
-}
     
-function verificaUrl() {
-    const string = document.querySelector('input').value;
-    try {
-     let url = new URL(string);
-     console.log("Valid URL!");
-   } catch(err) {
-       console.log("Invalid URL!");
-   }
-   verificaSeleçao()
-   isImage(string)
-   console.log(isImage());
-   console.log(isImage(string));
-} 
-
-function isImage(string) {
-    return /^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(string);
-  }
-
+}
 
 function selecionarModelo(seletor){
     modeloEscolhido = seletor.innerHTML;
@@ -54,7 +41,7 @@ function selecionarModelo(seletor){
     if( botaoSelecionado !== null ){
         botaoSelecionado.classList.remove('selecionado');
     }
-    const botao = document.querySelector(seletor);
+    const botao = document.querySelector(`.${seletor}`);
     botao.classList.add('selecionado');
     verificaSeleçao()
 }
@@ -65,7 +52,7 @@ function selecionarGola(seletor){
     if( botaoSelecionado !== null ){
         botaoSelecionado.classList.remove('selecionado');
     }
-    const botao = document.querySelector(seletor);
+    const botao = document.querySelector(`.${seletor}`);
     botao.classList.add('selecionado');
     verificaSeleçao()
 }
@@ -76,7 +63,118 @@ function selecionarTecido(seletor){
     if( botaoSelecionado !== null ){
         botaoSelecionado.classList.remove('selecionado');
     }
-    const botao = document.querySelector(seletor);
+    const botao = document.querySelector(`.${seletor}`);
     botao.classList.add('selecionado');
     verificaSeleçao()
 }
+
+function verificaUrl() {
+    const string = document.querySelector('input').value;
+    try {
+     let url = new URL(string);
+     console.log("Valida URL!");
+   } catch(err) {
+       console.log("Invalida URL!");
+   }
+   verificaSeleçao()
+   isImage(string)
+   enviarSelecao()
+} 
+
+function isImage(string) {
+    return /^https?:\/\/.+\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(string);
+}
+
+function enviarSelecao(){
+    const model = document.querySelector('.container-modelo .selecionado').classList;
+    const neck = document.querySelector('.container-gola .selecionado').classList;
+    const material = document.querySelector('.container-tecido .selecionado').classList;
+
+    const selecao = {
+        "model": model[0],
+        "neck": neck[0],
+        "material": material[0],
+        "image": input.value,
+        "owner": usuario,
+        "author": usuario
+    }
+    //console.log(selecao);
+
+    const resposta = axios.post('https://mock-api.driven.com.br/api/v4/shirts-api/shirts',selecao);
+    resposta.then(renderizarSucesso);
+    resposta.catch(erroEnviarPedido);
+}
+
+function renderizarSucesso(){
+    const sucesso = document.querySelector('.container-selecionar');
+    sucesso.innerHTML = `
+    <div class="sucesso">
+        Pedido feito com sucesso
+    </div>
+    <div>
+        <img class="imagem-sucesso" src="${input.value}" />
+    </div>`;
+    //setInterval(window.location.reload(),100000);
+}
+
+function erroEnviarPedido(erro){
+    console.log(erro)
+    if ( erro.response.status === 422){
+        const errado = document.querySelector('.container-selecionar');
+        errado.innerHTML = `
+        <div class="sucesso">
+            Algo deu errado!
+        </div>
+        <div>
+            <img class="imagem-sucesso" src="image 1.png" />
+        </div>`;
+        //setTimeout(window.location.reload(),10000);
+    }
+}
+
+function buscarPedidos(){
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/shirts-api/shirts');
+
+    promise.then(pedidosRecebidos);
+
+    promise.catch(erroBuscarPedidos);
+}
+buscarPedidos()
+
+function erroBuscarPedidos(erroBusca){
+
+    console.log(erroBusca)
+
+}
+
+let pedidos = [];
+
+function renderizarPedidos(){
+
+    const ultimoPedido= document.querySelector('.pedidos');
+
+    for(let i = 0 ; i < pedidos.lenght ; i++){
+
+        let pedido = pedidos[i];
+
+        ultimoPedido.innerHTML += `
+            <div>
+                <img class="imagem-sucesso" src="${pedido.input}" />
+            </div>
+            <div>
+                Criador: ${pedido.owner}
+            </div>
+        `;
+    }
+}
+
+function pedidosRecebidos(res){
+    console.log(res.data)
+
+    pedidos = res.data;
+
+    renderizarPedidos()
+}
+
+//const ultimoPedido = document.querySelector('.pedidos ');
